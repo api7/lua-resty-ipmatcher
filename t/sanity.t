api7/lua-resty-ipmatcher

commit eab27a27d98f4794279b34f389141f68d26fdf9e

feature: suppored matching binary format IP (#6)
The `match_bin` could be 30% faster than `match`.

resty/ipmatcher.lua
@@ -8,6 +8,7 @@ local ipairs      = ipairs
 local pairs       = pairs
 local ffi         = require "ffi"
 local ffi_cdef    = ffi.cdef
+local ffi_copy    = ffi.copy
 local ffi_new     = ffi.new
 local C           = ffi.C
 local insert_tab  = table.insert
@@ -54,6 +55,19 @@ do
 end
 _M.parse_ipv4 = parse_ipv4
 
+local parse_bin_ipv4
+do
+    local inet = ffi_new("unsigned int [1]")
+
+    function parse_bin_ipv4(ip)
+        if not ip or #ip ~= 4 then
+            return false
+        end
+
+        ffi_copy(inet, ip, 4)
+        return C.ntohl(inet[0])
+    end
+end
 
 local parse_ipv6
 do
@@ -83,6 +97,24 @@ do
     end
 end
 _M.parse_ipv6 = parse_ipv6
+
+local parse_bin_ipv6
+do
+    local inets = ffi_new("unsigned int [4]")
+
+    function parse_bin_ipv6(ip)
+        if not ip or #ip ~= 16 then
+            return false
+        end
+
+        ffi_copy(inets, ip, 16)
+        local inets_arr = new_tab(4, 0)
+        for i = 0, 3 do
+            insert_tab(inets_arr, C.ntohl(inets[i]))
+        end
+        return inets_arr
+    end
+end
 
 
 local mt = {__index = _M}
@@ -155,14 +187,13 @@ function _M.new(ips)
         if inet_ipv4 then
             ip_addr_mask = ip_addr_mask or 32
             if ip_addr_mask == 32 then
-                parsed_ipv4s[ip_addr] = true
+                parsed_ipv4s[inet_ipv4] = true
 
             else
                 local valid_inet_addr = bit.rshift(inet_ipv4, 32 - ip_addr_mask)
 
-                parsed_ipv4s[ip_addr_mask] = parsed_ipv4s[ip_addr_mask] or {}
-                parsed_ipv4s[ip_addr_mask][valid_inet_addr] = true
-                parsed_ipv4s_mask[ip_addr_mask] = true
+                parsed_ipv4s_mask[ip_addr_mask] = parsed_ipv4s[ip_addr_mask] or {}
+                parsed_ipv4s_mask[ip_addr_mask][valid_inet_addr] = true
                 log_info("ipv4 mask: ", ip_addr_mask,
                          " valid inet: ", valid_inet_addr)
             end
@@ -173,23 +204,22 @@ function _M.new(ips)
             ip_addr_mask = ip_addr_mask or 128
             if ip_addr_mask == 128 then
                 parsed_ipv6s[ip_addr] = true
-
-            else
-                parsed_ipv6s[ip_addr_mask] = parsed_ipv6s[ip_addr_mask] or {}
-
-                local inets_idxs = gen_ipv6_idxs(inets_ipv6, ip_addr_mask)
-                local node = parsed_ipv6s[ip_addr_mask]
-                for i, inet in ipairs(inets_idxs) do
-                    if i == #inets_idxs then
-                        node[inet] = true
-                    elseif not node[inet] then
-                        node[inet] = {}
-                        node = node[inet]
-                    end
-                end
-
-                parsed_ipv6s_mask[ip_addr_mask] = true
             end
+
+            parsed_ipv6s[ip_addr_mask] = parsed_ipv6s[ip_addr_mask] or {}
+
+            local inets_idxs = gen_ipv6_idxs(inets_ipv6, ip_addr_mask)
+            local node = parsed_ipv6s[ip_addr_mask]
+            for i, inet in ipairs(inets_idxs) do
+                if i == #inets_idxs then
+                    node[inet] = true
+                elseif not node[inet] then
+                    node[inet] = {}
+                    node = node[inet]
+                end
+            end
+
+            parsed_ipv6s_mask[ip_addr_mask] = true
         end
 
         if not inet_ipv4 and not inets_ipv6 then
@@ -219,49 +249,40 @@ function _M.new(ips)
 end
 
 
-function _M.match(self, ip)
-    local inet_ipv4 = parse_ipv4(ip)
-    if inet_ipv4 then
-        local ipv4s = self.ipv4
-        if ipv4s[ip] then
-            return true
-        end
-
-        for _, mask in ipairs(self.ipv4_mask_arr) do
-            if mask == 0 then
-                return true -- match any ip
-            end
-
-            local valid_inet_addr = bit.rshift(inet_ipv4, 32 - mask)
-
-            log_info("ipv4 mask: ", mask,
-                     " valid inet: ", valid_inet_addr)
-
-            if ipv4s[mask][valid_inet_addr] then
-                return true
-            end
-        end
-
-        return false
-    end
-
-    local inets_ipv6 = parse_ipv6(ip)
-    if not inets_ipv6 then
-        return false, "invalid ip address, not ipv4 and ipv6"
-    end
-
-    local ipv6s = self.ipv6
-    if ipv6s[ip] then
+local function match_ipv4(self, ip)
+    local ipv4s = self.ipv4
+    if ipv4s[ip] then
         return true
     end
 
+    local ipv4_mask = self.ipv4_mask
+    for _, mask in ipairs(self.ipv4_mask_arr) do
+        if mask == 0 then
+            return true -- match any ip
+        end
+
+        local valid_inet_addr = bit.rshift(ip, 32 - mask)
+
+        log_info("ipv4 mask: ", mask,
+                 " valid inet: ", valid_inet_addr)
+
+        if ipv4_mask[mask][valid_inet_addr] then
+            return true
+        end
+    end
+
+    return false
+end
+
+local function match_ipv6(self, ip)
+    local ipv6s = self.ipv6
     for _, mask in ipairs(self.ipv6_mask_arr) do
         if mask == 0 then
             return true -- match any ip
         end
 
         local node = ipv6s[mask]
-        local inet_idxs = gen_ipv6_idxs(inets_ipv6, mask)
+        local inet_idxs = gen_ipv6_idxs(ip, mask)
         for _, inet in ipairs(inet_idxs) do
             if not node[inet] then
                 break
@@ -275,6 +296,40 @@ function _M.match(self, ip)
     end
 
     return false
+end
+
+function _M.match(self, ip)
+    local inet_ipv4 = parse_ipv4(ip)
+    if inet_ipv4 then
+        return match_ipv4(self, inet_ipv4)
+    end
+
+    local inets_ipv6 = parse_ipv6(ip)
+    if not inets_ipv6 then
+        return false, "invalid ip address, not ipv4 and ipv6"
+    end
+
+    local ipv6s = self.ipv6
+    if ipv6s[ip] then
+        return true
+    end
+
+    return match_ipv6(self, inets_ipv6)
+end
+
+
+function _M.match_bin(self, bin_ip)
+    local inet_ipv4 = parse_bin_ipv4(bin_ip)
+    if inet_ipv4 then
+        return match_ipv4(self, inet_ipv4)
+    end
+
+    local inets_ipv6 = parse_bin_ipv6(bin_ip)
+    if not inets_ipv6 then
+        return false, "invalid ip address, not ipv4 and ipv6"
+    end
+
+    return match_ipv6(self, inets_ipv6)
 end
 
 

t/sanity.t
@@ -174,3 +174,145 @@ false
 GET /t
 --- no_error_log
 [error]
+
+
+
+=== TEST 7: match binary ip
+This test requires building Nginx with --with-http_realip_module
+--- config
+    location /foo {
+        set_real_ip_from  127.0.0.1;
+        content_by_lua_block {
+            ngx.log(ngx.INFO, ngx.var.http_x_real_ip, " ", ngx.var.binary_remote_addr)
+            ngx.print(ngx.var.binary_remote_addr)
+        }
+    }
+    location /t {
+        content_by_lua_block {
+            local function get_bin_ip(ip)
+                local sock = ngx.socket.tcp()
+                sock:settimeout(500)
+
+                local ok, err = sock:connect("127.0.0.1", $TEST_NGINX_SERVER_PORT)
+                if not ok then
+                    ngx.log(ngx.ERR, "failed to connect: ", err)
+                    return
+                end
+
+                local req = "GET /foo HTTP/1.0\r\nHost: test.com\r\nConnection: close\r\nX-Real-IP:" .. ip .. "\r\n\r\n"
+                local bytes, err = sock:send(req)
+                if not bytes then
+                    ngx.log(ngx.ERR, "failed to send http request: ", err)
+                    return
+                end
+
+                -- skip http header
+                while true do
+                    local data, err, _ = sock:receive('*l')
+                    if err then
+                        ngx.log(ngx.ERR, 'unexpected error occurs when receiving http head: ' .. err)
+                        return
+                    end
+                    if #data == 0 then -- read last line of head
+                        break
+                    end
+                end
+
+                local data, err = sock:receive('*a')
+                sock:close()
+                if not data then
+                    ngx.log(ngx.ERR, "failed to receive body: ", err)
+                end
+                return data
+            end
+
+            local ip = require("resty.ipmatcher").new({
+                "127.0.0.1",
+                "192.168.0.0/16",
+                "::1",
+                "fe80::/8",
+            })
+            local cases = {
+                {ip = "127.0.0.1", matched = true},
+                {ip = "127.0.0.2", matched = false},
+                {ip = "192.168.0.22", matched = true},
+                {ip = "182.168.0.22", matched = false},
+                {ip = "::1", matched = true},
+                {ip = "::2", matched = false},
+                {ip = "fe80::1", matched = true},
+            }
+            for _, case in ipairs(cases) do
+                local res = ip:match_bin(get_bin_ip(case.ip))
+                if res ~= case.matched then
+                    ngx.say("unexpected result for ", case.ip)
+                end
+            end
+            ngx.say("ok")
+        }
+    }
+--- request
+GET /t
+--- no_error_log
+[error]
+--- response_body
+ok
+
+
+
+=== TEST 8: zero mask
+--- config
+    location /t {
+        content_by_lua_block {
+            local ip = require("resty.ipmatcher").new({
+                "::/0",
+                "0.0.0.0/0",
+            })
+
+            ngx.say(ip:match("127.0.0.1"))
+            ngx.say(ip:match("0.0.0.0"))
+            ngx.say(ip:match("fe81::"))
+            ngx.say(ip:match("ff80::"))
+        }
+    }
+--- request
+GET /t
+--- no_error_log
+[error]
+--- response_body
+true
+true
+true
+true
+
+
+
+=== TEST 9: ipv6 special notation
+--- config
+    location /t {
+        content_by_lua_block {
+            local ip = require("resty.ipmatcher").new({
+                "2001:0db8:85a3:0000:0000:8a2e:0370:7334",
+            })
+            ngx.say(ip:match("2001:0db8:85a3:0000:0000:8a2e:0370:7334"))
+            -- zero in some occasions can be omitted
+            ngx.say(ip:match("2001:db8:85a3:0:0:8a2e:370:7334"))
+            ngx.say(ip:match("2001:db8:85a3::8a2e:370:7334"))
+
+            local ip = require("resty.ipmatcher").new({
+                "::ffff:192.0.2.128",
+            })
+            ngx.say(ip:match("::ffff:192.0.2.128"))
+            -- ipv4-mapped address
+            ngx.say(ip:match("::ffff:c000:0280"))
+        }
+    }
+--- request
+GET /t
+--- no_error_log
+[error]
+--- response_body
+true
+true
+true
+true
+true

commit dcb0d52558f869b0d5a96030877f2e61bc7e08a1

bugfix: avoid overriding ipv4 rule with the same mask.
This bug is introduced in commit eab27a27d98f4794279b34f389141f68d26fdf9e,
which affects v0.5.

resty/ipmatcher.lua
@@ -211,7 +211,7 @@ local function new(ips, with_value)
             else
                 local valid_inet_addr = bit.rshift(inet_ipv4, 32 - ip_addr_mask)
 
-                parsed_ipv4s_mask[ip_addr_mask] = parsed_ipv4s[ip_addr_mask] or {}
+                parsed_ipv4s_mask[ip_addr_mask] = parsed_ipv4s_mask[ip_addr_mask] or {}
                 parsed_ipv4s_mask[ip_addr_mask][valid_inet_addr] = value
                 log_info("ipv4 mask: ", ip_addr_mask,
                          " valid inet: ", valid_inet_addr)

t/sanity.t
@@ -385,3 +385,26 @@ GET /t
 2
 1
 1
+
+
+
+=== TEST 12: bug: ipv4 address overrided with the same mask
+--- config
+    location /t {
+        content_by_lua_block {
+            local ip = require("resty.ipmatcher").new({
+                "192.168.0.0/16",
+                "192.0.0.0/16",
+            })
+
+            ngx.say(ip:match("192.168.1.1"))
+            ngx.say(ip:match("192.0.1.100"))
+        }
+    }
+--- request
+GET /t
+--- no_error_log
+[error]
+--- response_body
+true
+true